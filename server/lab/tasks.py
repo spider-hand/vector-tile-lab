64acd7b900990c2e4b08b6a04f17c458f00da471
spider-hand <creative.spider.hand@gmail.com>
2025-09-22 00:42:44 +0900

    chore: set up tippecanoe

--- server/lab/tasks.py
+++ server/lab/tasks.py
@@ -1,7 +1,9 @@
+import subprocess
 from celery import shared_task
 
 
 @shared_task
 def process_uploaded_geojson(dataset_name):
     print(f"Start to process {dataset_name} ...")
-    return f"Start to process {dataset_name} ..."
+    result = subprocess.run(["tippecanoe", "--version"], capture_output=True, text=True)
+    print(result.stderr.strip())
